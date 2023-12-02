5b1fbffc265ee5caa3ee8cb29c61460e69de6bc8
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2023-12-02 16:36:05 +0700

    Update create permission

--- migrations/index.ts
+++ migrations/index.ts
@@ -2,6 +2,7 @@ import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 import { AddTableMail1700555976577 } from './1700555976577-AddTableMail';
 import { CreateTableTemplate1701400687844 } from './1701400687844-CreateTableTemplate';
+import { AddMailPermission1701509428731 } from './1701509428731-AddMailPermission';
 
 config();
 
@@ -13,5 +14,5 @@ export default new DataSource({
   database: process.env.PG_DATABASE,
   port: +process.env.PG_PORT,
   entities: [],
-  migrations: [AddTableMail1700555976577, CreateTableTemplate1701400687844],
+  migrations: [AddTableMail1700555976577, CreateTableTemplate1701400687844, AddMailPermission1701509428731],
 });
